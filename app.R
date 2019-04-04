57e59020c1140b1725c916b19b32f1617a55fa64
Emily Bellis <emilysbellis@gmail.com>
2019-04-04 10:50:15 -0400

    Add files via upload
    add ability to switch between sorghum-only and all occurrence models; delay render functions allow time for user to enter lat/lon numeric input

--- app.R
+++ app.R
@@ -14,12 +14,12 @@ library(raster)
 library(maps)
 
 all <- raster('/Users/emilywork/Desktop/ENM/distModel7.30/preds.all.tif') ##raster layer output from species distribution model
+sorg <- raster('/Users/emilywork/Desktop/ENM/distModel7.30/preds.sorg.tif', header=T)
 shgeo <- read.csv('/Users/emilywork/Desktop/ENM/Final_occurrence_data/curated_occ_ESB_7.26.18.csv', header=T,stringsAsFactors=F) ##1369 S. hermonthica occurrences
 data(worldMapEnv)
 
 # Define UI/layout ----
 ui <- fluidPage(theme=shinytheme("spacelab"),
-
   # App title ----
   titlePanel(HTML("<em>Striga hermonthica</em> distribution model")),
  
@@ -34,6 +34,8 @@ ui <- fluidPage(theme=shinytheme("spacelab"),
                   value = 34.76),
       checkboxInput(inputId = "occs", "Plot known occurrence records?", value=FALSE
       		),
+      checkboxInput(inputId = "sorgmod", "Use sorghum-only model?", value=FALSE
+      		),
       		
       # Output panel
       h4("Habitat suitability score at location of interest is", textOutput("hs"))
@@ -42,36 +44,47 @@ ui <- fluidPage(theme=shinytheme("spacelab"),
   # Main panel for displaying outputs ----
   mainPanel(
   	plotOutput("baseplot"),
-  	h4("Maxent species distribution model based on 1050 S. hermonthica occurrences and 8 environmental variables (annual rainfall, soil nitrogen, soil phosphorus, soil clay fraction, mean temperature of the wettest quarter, isothermality, topographic wetness index, and PET)."),
-  	h4("Additional information about the model can be found in Bellis et al. (submitted)."),
-  	h4("Plot may take a minute or so to load!")
+    p(("Maxent species distribution model based on") ,em("S. hermonthica "),("occurrences and 8 environmental variables (annual rainfall, soil nitrogen, soil phosphorus, soil clay fraction, mean temperature of the wettest quarter, isothermality, topographic wetness index, and PET). The default model is based on 1050 parasite occurrence records. The sorghum-only model excludes parasites on unknown or non-sorghum hosts, leaving a subset of 262 parasite occurrence records.")),
+  	p("Additional information about the models can be found in Bellis et al. (in prep)."),
+  	p("Plot may take a minute or so to load!")
   )
 )
 
 # Define server logic to plot things with reactivity
 server <- function(input, output) {
-	
-	#the first reactive thing is my main plot
+	#put all the reactive expressions up here
+		getbasemap <- reactive({ #this is in a reactive expression because it is shared by both the plot and function that extracts the value of the model
+			if(input$sorgmod==TRUE){  #this part responds to the check box if user wants the sorghum-only model
+				sorg
+			} else {all}
+		})
+		Lat <- reactive({input$lat})
+		Lon <- reactive({input$lon})
+		Lat_d <- debounce(Lat, 2000) #slow this down to 3 seconds allow user to type
+		Lon_d <- debounce(Lon, 2000) #slow this down to allow user to type
+		
+	#the first render thing I want to do is my main plot
 	output$baseplot <- renderPlot({
 		map(database="world",lwd=0.5, col="grey50")
-		#, xlim=c(-10,70),ylim=c(-45,60),axes=TRUE)
-		plot(all, add=T, legend=F)
+		basemap <- getbasemap()
+		plot(basemap, add=T, legend=F)
 		map(database="world", add=T, lwd=0.5, col="grey50")
 		
 		if(input$occs==TRUE){  #this part responds to the check box if user wants to plot all the S. hermonthica occurrences
 			points(shgeo$lon, shgeo$lat, col='black',cex=0.1, pch=20)
 		}
 		
-		points(x=input$lon, y=input$lat, col='black', pch='X') #mark location in side panel with an X
+		points(x=Lon_d(), y=Lat_d(), col='black', pch='X') #mark location in side panel with an X
 		scalebar(5000, xy = c(100,-57), type = "bar", divs = 2, lonlat = TRUE)
-		plot(all, legend.only=T, add=T, smallplot=c(0.2,0.25, 0.3,0.45))
+		plot(basemap, legend.only=T, add=T, smallplot=c(0.2,0.25, 0.3,0.45))
 		text(-120,-7, "Habitat suitability")
 		text(100,-62, "km")
 	})
 	
-	#the second reactive thing is extracting the value of the model at any gps location
+	#the second render thing is extracting the value of the model at any gps location
 	output$hs <- renderText({
-		extract(all,cbind(input$lon,input$lat))
+		basemap <- getbasemap()
+		extract(basemap,cbind(Lon_d(),Lat_d()))
 	})
 }
 
